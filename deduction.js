87ade268eefd4cf268711afab505346f4c038bdf
Jianni Hu <hujianni.art@qq.com>
2022-12-31 15:06:08 -0500

    update z-index and footer credits

--- deduction.js
+++ deduction.js
@@ -25,6 +25,7 @@ const evidenceArray = [
         let game = document.getElementById('game');
         let newimg = document.createElement('img');
         newimg.style.position = 'absolute';
+        newimg.style.zIndex = '0';
         newimg.src = evidenceArray[num];
         newimg.width = 100;
         newimg.setAttribute("id",`${index}`);
@@ -32,7 +33,7 @@ const evidenceArray = [
         index++;
         // set position here 
         newimg.style.left = position.x;
-        newimg.style.top = position.y + 50;
+        newimg.style.top = position.y;
 
         // add new Child image to game
         newimg.style.zIndex = "0";
@@ -89,3 +90,49 @@ const evidenceArray = [
     
 
     
+ footerCredits = [
+    {href:"https://www.flaticon.com/free-icons/evidence",
+    title:"evidence icons",
+    text:"Evidence icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/blood",
+    title:"blood icons",
+    text:"Blood icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/clock",
+    title:"clock icons",
+    text:"Blood icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/blood",
+    title:"blood icons",
+    text:"Clock icons created by surang - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/cigarette-butt",
+    title:"cigarette butt icons",
+    text:"Cigarette butt icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/evidences",
+    title:"evidences icons",
+    text:"Evidences icons created by smalllikeart - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/feet",
+    title:"feet icons",
+    text:"Feet icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/fingerprint",
+    title:"fingerprint icons",
+    text:"Fingerprint icons created by Freepik - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/forensic-science",
+    title:"forensic science icons",
+    text:"Forensic science icons created by Khoirul Huda - Flaticon"},
+    {href:"https://www.flaticon.com/free-icons/morgue",
+    title:"morgue icons",
+    text:"Morgue icons created by Flat Icons - Flaticon"}
+]
+
+ const footer = document.getElementById("footer");
+ footerCredits.forEach(element => {
+    let link = element.href;
+    let title = element.title;
+    let text = element.text;
+    let credit = document.createElement("span");
+    let creditLink = document.createElement("a");
+    credit.appendChild(creditLink);
+    creditLink.setAttribute("href",link);
+    creditLink.setAttribute("title",title);
+    creditLink.innerHTML = text;
+    footer.appendChild(credit);
+});
